ffb2acf73fad36e24ddc786103a1391b1883ba8d
Thomas <semoir.dense-0h@icloud.com>
2024-06-09 04:48:26 +0000

    Update CMakeLists.txt and add Constants.hpp (#2)
    define PI in constant.hpp
    
    ------------------------------------
    
    * update cmake lists required version
    
    * Add Constants.hpp file with PI constant
    
    * Refactor CMakeLists.txt for Math library
    
    * update cmakelists
    
    * Update UtilsCPP subproject commit
    
    * chore: Update .gitignore and CMakeLists.txt
    
    * chore: Set target properties for UtilsCPP and gtest in CMakeLists.txt

---
+++ include/Math/Constants.hpp
@@ -0,0 +1,20 @@
+/*
+ * ---------------------------------------------------
+ * Constants.hpp
+ *
+ * Author: Thomas Choquet <thomas.publique@icloud.com>
+ * Date: 2024/06/03 19:15:32
+ * ---------------------------------------------------
+ */
+
+#ifndef CONSTANTS_HPP
+# define CONSTANTS_HPP
+
+#ifndef PI
+    #define PI 3.14159265358979323846264338327950288
+    #ifndef M_PI
+        #define M_PI PI
+    #endif
+#endif
+
+#endif // CONSTANTS_HPP
